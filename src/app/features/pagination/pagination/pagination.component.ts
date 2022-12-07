e4f37624e9e150ff536e45053e231e9f7a94bf6c
guygauravtyagi <gtyagig786@gmail.com>
2022-12-07 19:40:41 +0530

    Still suffering custom pagination module

--- src/app/features/pagination/pagination/pagination.component.ts
+++ src/app/features/pagination/pagination/pagination.component.ts
@@ -1,12 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnChanges {
 
   @Input() paginationData!: any[];
+  @Input() rowsPerPage = 10;
+  @Input() pageNumber = 1;
+
+  @Output() globalSelectionEvent: EventEmitter<any[]> = new EventEmitter<any[]>();
+  @Output() valueSelectionEvent: EventEmitter<any[]> = new EventEmitter<any[]>();
+
+  visibleList: any[] = [];
+
+  constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.paginationData = changes['paginationData'].currentValue;
+    if (changes['rowsPerPage']?.currentValue) this.rowsPerPage = changes['rowsPerPage'].currentValue;
+    if (changes['pageNumber']?.currentValue) this.pageNumber = changes['pageNumber'].currentValue;
+    if (this.paginationData && this.paginationData.length > 0) this.setVisibleList(this.paginationData, this.pageNumber, this.rowsPerPage);
+  }
+
+  setVisibleList(pageData: any[], pageNumber: number, rowsPerPage: number) {
+    this.visibleList = pageData.filter((ele, index) => index < (pageNumber * rowsPerPage) && index >= ((pageNumber - 1) * rowsPerPage));
+  }
 
 }
--- src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -12,6 +12,7 @@ export class DashboardComponent {
 
   public members$!: Observable<Member[]>;
   public members!: Member[];
+  public pageNumber = 1;
 
   constructor(private mainService: MainService) {
     this.members$ = this.mainService.getData()
